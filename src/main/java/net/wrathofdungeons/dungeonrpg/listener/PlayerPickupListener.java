47af7683816c6012937f0bad957f35aef8189654
Zeryther <zeryther@gmail.com>
2017-09-13 17:23:23 +0200

    Update item on pickup

---
+++ src/main/java/net/wrathofdungeons/dungeonrpg/listener/PlayerPickupListener.java
@@ -0,0 +1,27 @@
+package net.wrathofdungeons.dungeonrpg.listener;
+
+import net.wrathofdungeons.dungeonrpg.items.CustomItem;
+import net.wrathofdungeons.dungeonrpg.user.GameUser;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerPickupItemEvent;
+
+public class PlayerPickupListener implements Listener {
+    @EventHandler
+    public void onPickup(PlayerPickupItemEvent e){
+        Player p = e.getPlayer();
+
+        if(GameUser.isLoaded(p)){
+            GameUser u = GameUser.getUser(p);
+
+            if(e.getItem().getItemStack() != null){
+                CustomItem item = CustomItem.fromItemStack(e.getItem().getItemStack());
+
+                if(item != null){
+                    e.getItem().setItemStack(item.build(p));
+                }
+            }
+        }
+    }
+}
