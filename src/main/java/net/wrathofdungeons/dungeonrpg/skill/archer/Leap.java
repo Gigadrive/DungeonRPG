f94cff729271d2d191ee854bb0f465fb0d17a013
Zeryther <zeryther@gmail.com>
2017-09-19 11:12:23 +0200

    Added leap skill

--- src/main/java/net/wrathofdungeons/dungeonrpg/skill/archer/Leap.java
+++ src/main/java/net/wrathofdungeons/dungeonrpg/skill/archer/Leap.java
@@ -1,8 +1,10 @@
 package net.wrathofdungeons.dungeonrpg.skill.archer;
 
+import net.wrathofdungeons.dungeonapi.util.ParticleEffect;
 import net.wrathofdungeons.dungeonrpg.skill.Skill;
 import net.wrathofdungeons.dungeonrpg.skill.SkillType;
 import net.wrathofdungeons.dungeonrpg.user.RPGClass;
+import org.bukkit.Sound;
 import org.bukkit.entity.Player;
 
 public class Leap implements Skill {
@@ -28,6 +30,8 @@ public class Leap implements Skill {
 
     @Override
     public void execute(Player p) {
-
+        p.getWorld().playSound(p.getEyeLocation(), Sound.FIREWORK_LAUNCH, 1F, 1F);
+        ParticleEffect.CLOUD.display(0.05F, 0.05F, 0.05F, 0.05F, 120, p.getLocation().add(0.0, -1.0, 0.0), 900);
+        p.setVelocity(p.getLocation().getDirection().multiply(1.5).setY(1));
     }
 }
