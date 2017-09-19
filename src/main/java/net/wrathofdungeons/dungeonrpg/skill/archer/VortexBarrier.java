3916b28c4289d55ca08547b8268fca507c3c5c44
Zeryther <zeryther@gmail.com>
2017-09-19 16:31:02 +0200

    Added speed effect to vortex barrier skill

--- src/main/java/net/wrathofdungeons/dungeonrpg/skill/archer/VortexBarrier.java
+++ src/main/java/net/wrathofdungeons/dungeonrpg/skill/archer/VortexBarrier.java
@@ -10,10 +10,13 @@ import net.wrathofdungeons.dungeonrpg.skill.SkillType;
 import net.wrathofdungeons.dungeonrpg.user.GameUser;
 import net.wrathofdungeons.dungeonrpg.user.RPGClass;
 import net.wrathofdungeons.dungeonrpg.util.WorldUtilities;
+import org.bukkit.ChatColor;
 import org.bukkit.Location;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.LivingEntity;
 import org.bukkit.entity.Player;
+import org.bukkit.potion.PotionEffect;
+import org.bukkit.potion.PotionEffectType;
 import org.bukkit.scheduler.BukkitRunnable;
 
 public class VortexBarrier implements Skill {
@@ -51,6 +54,13 @@ public class VortexBarrier implements Skill {
             u.getSkillValues().vortexBarrierLoc = p.getLocation().clone();
             int range = 7;
 
+            PotionEffect speed = new PotionEffect(PotionEffectType.SPEED,3*60*20,1,true,true);
+
+            if(!p.hasPotionEffect(PotionEffectType.SPEED)){
+                p.addPotionEffect(speed);
+                p.sendMessage(ChatColor.GREEN + p.getName() + " has given you speed.");
+            }
+
             for(Entity e : p.getNearbyEntities(7,7,7)){
                 if(e instanceof LivingEntity){
                     LivingEntity ent = (LivingEntity)e;
@@ -60,7 +70,22 @@ public class VortexBarrier implements Skill {
                         ent.setVelocity(ent.getLocation().toVector().subtract(u.getSkillValues().vortexBarrierLoc.toVector()).normalize().multiply(0.75).setY(1.25));
                     } else {
                         if(ent instanceof Player){
-                            // TODO: handle duels
+                            Player p2 = (Player)ent;
+
+                            if(false){
+                                // TODO: handle duels
+                            } else {
+                                if(GameUser.isLoaded(p2)){
+                                    GameUser u2 = GameUser.getUser(p2);
+
+                                    if(u2.getCurrentCharacter() != null){
+                                        if(!p2.hasPotionEffect(PotionEffectType.SPEED)){
+                                            p2.addPotionEffect(speed);
+                                            p.sendMessage(ChatColor.GREEN + p.getName() + " has given you speed.");
+                                        }
+                                    }
+                                }
+                            }
                         }
                     }
                 }
