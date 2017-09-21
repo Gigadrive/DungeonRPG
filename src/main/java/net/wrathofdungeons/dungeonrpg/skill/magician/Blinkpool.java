c2f2e491a68e854882d8ca87842eacb656eba72d
Zeryther <zeryther@gmail.com>
2017-09-21 23:16:16 +0200

    Added Blinkpool skill

--- src/main/java/net/wrathofdungeons/dungeonrpg/skill/magician/Blinkpool.java
+++ src/main/java/net/wrathofdungeons/dungeonrpg/skill/magician/Blinkpool.java
@@ -1,9 +1,17 @@
 package net.wrathofdungeons.dungeonrpg.skill.magician;
 
+import com.sun.xml.internal.bind.v2.schemagen.xmlschema.Particle;
+import net.wrathofdungeons.dungeonapi.util.ParticleEffect;
 import net.wrathofdungeons.dungeonrpg.skill.Skill;
 import net.wrathofdungeons.dungeonrpg.skill.SkillType;
 import net.wrathofdungeons.dungeonrpg.user.RPGClass;
+import org.bukkit.ChatColor;
+import org.bukkit.Location;
+import org.bukkit.Material;
+import org.bukkit.block.Block;
 import org.bukkit.entity.Player;
+import org.bukkit.util.BlockIterator;
+import org.bukkit.util.Vector;
 
 public class Blinkpool implements Skill {
     @Override
@@ -28,6 +36,45 @@ public class Blinkpool implements Skill {
 
     @Override
     public void execute(Player p) {
+        Location loc = p.getEyeLocation();
+        try {
+            BlockIterator bi = new BlockIterator(loc, 0.0, 15);
 
+            Location lastLoc = null;
+            while(bi.hasNext()){
+                Block b = bi.next();
+
+                if(b.getType() == Material.AIR){
+                    lastLoc = b.getLocation();
+                }
+            }
+
+            if(lastLoc == null) lastLoc = loc;
+
+            int c = (int)Math.ceil(loc.distance(lastLoc) / 2F) - 1;
+            if(c > 0){
+                Vector v = lastLoc.toVector().subtract(loc.toVector()).normalize().multiply(2F);
+                Location l = loc.clone();
+                for (int i = 0; i < c; i++) {
+                    l.add(v);
+                    ParticleEffect.FLAME.display(0f,0f,0f,0.005f,3,l,600);
+                }
+            }
+
+            double y = lastLoc.getY();
+
+            while((lastLoc.getBlock() != null && lastLoc.getBlock().getType() != null && lastLoc.getBlock().getType() != Material.AIR)){
+                y++;
+                lastLoc.setY(y);
+            }
+
+            lastLoc.setY(y);
+            lastLoc.setYaw(loc.getYaw());
+            lastLoc.setPitch(loc.getPitch());
+
+            p.teleport(lastLoc);
+        } catch(IllegalStateException ex){
+            p.sendMessage(ChatColor.RED + "Teleport failed! Are you standing too close to something?");
+        }
     }
 }
