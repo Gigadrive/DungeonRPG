57ee3b39cff9dbacdc646b92cf647eb0cc9cce0e
Zeryther <zeryther@gmail.com>
2017-11-18 17:27:13 +0100

    Show message on needed item pickup

--- src/main/java/net/wrathofdungeons/dungeonrpg/listener/PlayerPickupListener.java
+++ src/main/java/net/wrathofdungeons/dungeonrpg/listener/PlayerPickupListener.java
@@ -3,7 +3,12 @@ package net.wrathofdungeons.dungeonrpg.listener;
 import net.wrathofdungeons.dungeonrpg.DungeonRPG;
 import net.wrathofdungeons.dungeonrpg.items.CustomItem;
 import net.wrathofdungeons.dungeonrpg.npc.CustomNPC;
+import net.wrathofdungeons.dungeonrpg.quests.Quest;
+import net.wrathofdungeons.dungeonrpg.quests.QuestObjective;
+import net.wrathofdungeons.dungeonrpg.quests.QuestProgressStatus;
+import net.wrathofdungeons.dungeonrpg.quests.QuestStage;
 import net.wrathofdungeons.dungeonrpg.user.GameUser;
+import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -38,6 +43,20 @@ public class PlayerPickupListener implements Listener {
                                 e.getItem().remove();
                                 p.getInventory().addItem(item.build(p));
                                 u.playItemPickupSound();
+
+                                for(Quest q : Quest.STORAGE.values()){
+                                    if(u.getCurrentCharacter().getStatus(q) == QuestProgressStatus.STARTED){
+                                        QuestStage stage = q.getStages()[u.getCurrentCharacter().getCurrentStage(q)];
+
+                                        if(stage != null){
+                                            for(QuestObjective o : stage.objectives){
+                                                if(o.itemToFind == item.getData().getId()){
+                                                    p.sendMessage(ChatColor.GRAY + "[Find " + ChatColor.stripColor(item.getData().getName()) + " " + ChatColor.WHITE + "(" + u.getAmountInInventory(item.getData()) + "/" + o.itemToFindAmount + ")" + ChatColor.GRAY + "]");
+                                                }
+                                            }
+                                        }
+                                    }
+                                }
                             } else {
                                 if(now-dropTime >= 30*1000 && !item.isUntradeable()){
                                     e.getItem().remove();
