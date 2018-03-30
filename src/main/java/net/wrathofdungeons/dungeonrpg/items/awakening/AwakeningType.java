b43255937e8a9de53375ddd2f870387c9fd0b2d0
Zeryther <zeryther@gmail.com>
2018-03-30 21:18:04 +0200

    Updated awakening minimums and maximums

--- src/main/java/net/wrathofdungeons/dungeonrpg/items/awakening/AwakeningType.java
+++ src/main/java/net/wrathofdungeons/dungeonrpg/items/awakening/AwakeningType.java
@@ -1,33 +1,32 @@
 package net.wrathofdungeons.dungeonrpg.items.awakening;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 
 public enum AwakeningType {
-    STR_BONUS(1,"STR Bonus","STR",true,false,-7,+7,AwakeningCategory.WEAPON_ARMOR),
-    STA_BONUS(2,"STA Bonus","STA",true,false,-7,+7,AwakeningCategory.WEAPON_ARMOR),
-    INT_BONUS(3,"INT Bonus","INT",true,false,-7,+7,AwakeningCategory.WEAPON_ARMOR),
-    DEX_BONUS(4,"DEX Bonus","DEX",true,false,-7,+7,AwakeningCategory.WEAPON_ARMOR),
-    AGI_BONUS(5,"AGI Bonus","AGI",true,false,-7,+7,AwakeningCategory.WEAPON_ARMOR),
-    DEFENSE(6,"Defense","DEF",false,true,-28,+34,AwakeningCategory.WEAPON_ARMOR),
+    STR_BONUS(1, "STR Bonus", "STR", true, false, -12, +12, AwakeningCategory.WEAPON_ARMOR),
+    STA_BONUS(2, "STA Bonus", "STA", true, false, -12, +12, AwakeningCategory.WEAPON_ARMOR),
+    INT_BONUS(3, "INT Bonus", "INT", true, false, -12, +12, AwakeningCategory.WEAPON_ARMOR),
+    DEX_BONUS(4, "DEX Bonus", "DEX", true, false, -12, +12, AwakeningCategory.WEAPON_ARMOR),
+    AGI_BONUS(5, "AGI Bonus", "AGI", true, false, -12, +12, AwakeningCategory.WEAPON_ARMOR),
+    DEFENSE(6, "Defense", "DEF", false, true, -28, +12, AwakeningCategory.WEAPON_ARMOR),
     ATTACK_DAMAGE(7,"Attack Damage","ATK",false,true,-25,+45,AwakeningCategory.WEAPON_ARMOR),
-    THORNS(8,"Thorns","THR",false,true,-12,+8,AwakeningCategory.WEAPON_ARMOR),
+    THORNS(8, "Thorns", "THR", false, true, -12, +4, AwakeningCategory.WEAPON_ARMOR),
     MELEE_DAMAGE(9,"Melee Damage","MDMG",false,true,-32,+56,AwakeningCategory.WEAPON_ARMOR),
     SKILL_DAMAGE(10,"Skill Damage","SDMG",false,true,-32,+56,AwakeningCategory.WEAPON_ARMOR),
     HP_REGENERATION(11,"HP Regeneration","HPREGEN",false,true,-26,+12,AwakeningCategory.WEAPON_ARMOR),
     MP_REGENERATION(12,"MP Regeneration","MPREGEN",false,true,-26,+12,AwakeningCategory.WEAPON_ARMOR),
     XP_BONUS(13,"XP Bonus","XP",false,true,-45,+45,AwakeningCategory.WEAPON_ARMOR),
-    LOOT_BONUS(14,"Loot Bonus","LOOT",false,true,-45,+45,AwakeningCategory.WEAPON_ARMOR),
+    LOOT_BONUS(14, "Loot Bonus", "LOOT", false, true, -45, +26, AwakeningCategory.WEAPON_ARMOR),
     HP_LEECH(15,"HP Leech","LEECH",false,true,-12,+6,AwakeningCategory.WEAPON_ARMOR),
     MP_LEECH(16,"MP Leech","MPLEECH",false,true,-12,+6,AwakeningCategory.WEAPON_ARMOR),
     DODGING(17,"Dodging","DODGE",false,true,-14,+19,AwakeningCategory.WEAPON_ARMOR),
-    FORTUNE(18,"Fortune","FOR",false,true,-3,+5,AwakeningCategory.WEAPON_ARMOR),
+    FORTUNE(18, "Fortune", "FOR", false, true, -3, +11, AwakeningCategory.WEAPON_ARMOR),
     ADOCH(19,"Critical Damage","ADOCH",false,true,-24,+34,AwakeningCategory.WEAPON_ARMOR),
     MINING_SPEED(20,"Mining Speed","MSPEED",true,false,-6,+6,AwakeningCategory.PICKAXE),
-    WALK_SPEED(21,"Walk Speed","SPEED",false,true,-32,+39,AwakeningCategory.WEAPON_ARMOR),
+    WALK_SPEED(21, "Walk Speed", "SPEED", false, true, -21, +14, AwakeningCategory.WEAPON_ARMOR),
     ADDITIONAL_HP(22,"HP","HP",false,true,-76,+81,AwakeningCategory.WEAPON_ARMOR),
     ADDITIONAL_MP(23,"MP","MP",false,true,-76,+81,AwakeningCategory.WEAPON_ARMOR),
-    CRIT_CHANCE(24,"Critical Chance","CRIT",false,true,-43,+51,AwakeningCategory.WEAPON_ARMOR);
+    CRIT_CHANCE(24, "Critical Chance", "CRIT", false, true, -39, +16, AwakeningCategory.WEAPON_ARMOR);
 
     private int id;
     private String displayName;
